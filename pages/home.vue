057a7ee0c16e26523602e0760452dceff0f92cb6
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-03 23:15:16 +0900

    各ページ作成

---
+++ pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-card>
+        <v-card-title>{{ this.$auth.$state.user.sub }}</v-card-title>
+        <v-card-title>{{ this.$auth.$state }}</v-card-title>
+        <v-btn @click="logout">logout</v-btn>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+    }
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col>
+      <div id="show-auth" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.loginForm()
+  },
+  methods: {
+    loginForm() {
+      this.$auth0.showLock('show-auth')
+    }
+  }
+}
+</script>
