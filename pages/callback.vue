8e69de309267efbe5d614a5d2ca98d3b8cac9657
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-18 19:27:42 +0900

    ログイン時にsecurd Controllerを叩くように変更

--- pages/callback.vue
+++ pages/callback.vue
@@ -7,7 +7,19 @@ export default {
   mounted() {
     this.$auth0.setTokenByQuery()
     this.$store.dispatch('logIn')
+    this.login()
     this.$router.replace('/')
+  },
+  methods: {
+    async login() {
+      const uri = "http://localhost:3000/api/v1/login"
+      const headers = { Authorization: `Bearer ${this.$auth0.getAccessToken()}` }
+      await this.$axios.get(uri, { headers: headers }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   }
 }
 </script>
