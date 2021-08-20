d2e8eb1786ba6b4b2c3d2437153956e27ba679f0
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-20 19:20:17 +0900

    メイン画面にランダムで一人ユーザーを表示

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <v-card>
         <div v-if="loggedIn">
           <v-card-text>ログイン中</v-card-text>
+          <v-card-title>{{ other_user }}</v-card-title>
           <v-card-text>{{ this.$auth0.getAccessToken() }}</v-card-text>
           <v-btn to="/logout" nuxt>ログアウト</v-btn>
         </div>
@@ -17,13 +18,24 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   mounted() {
-    console.log(this.$store.state.authentication.isAuth)
+    this.getOtherUser()
   },
   computed: {
     loggedIn() {
       return this.$auth0.isAuthenticated()
+    },
+    ...mapState({
+      other_user: state => state.user.other_user
+    })
+  },
+  methods: {
+    getOtherUser() {
+      const headers = { Authorization: `Bearer ${this.$auth0.getAccessToken()}` }
+      this.$store.dispatch('user/getOtherUser', { headers: headers })
     }
   }
 }
